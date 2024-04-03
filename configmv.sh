ec7b3918cd83164dec16e239b337cb01969e5435
zmeyka3310 <castella3310@gmail.com>
2024-04-03 23:28:22 +0300

    moved wallpapers to config/wallpapers

--- configmv.sh
+++
@@ -1,5 +0,0 @@
-cp -r ~/.config/hypr/ ~/repos/dotfiles-hyprland/.config/hypr/
-cp -r ~/.config/waybar/ ~/repos/dotfiles-hyprland/.config/
-cp -r ~/.config/wofi/ ~/repos/dotfiles-hyprland/.config/
-cp -r ~/.config/wallpapers/ ~/repos/dotfiles-hyprland/.config/
-cp -r ~/.bin/ ~/repos/dotfiles-hyprland
