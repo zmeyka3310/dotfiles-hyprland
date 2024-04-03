ddee5a73b3c4471e2e22f1144f9903ec50205a97
zmeyka3310 <castella3310@gmail.com>
2024-04-03 23:42:25 +0300

    updated the script to remove an autogenerated file for screenshotting

--- .bin/screenshotassist.sh
+++
@@ -1,1 +0,0 @@
-mv /tmp/image.png /home/zmeyka/Pictures/Screenshots/Screenshot\ from\ 2024-04-03\ 22-48-15.png
--- configmv.sh
+++ configmv.sh
@@ -3,3 +3,4 @@ cp -r ~/.config/waybar/ ~/repos/dotfiles-hyprland/.config/
 cp -r ~/.config/wofi/ ~/repos/dotfiles-hyprland/.config/
 cp -r ~/.config/wallpapers/ ~/repos/dotfiles-hyprland/.config/
 cp -r ~/.bin/ ~/repos/dotfiles-hyprland
+rm ~/repos/dotfiles-hyprland/.bin/screenshotassist.sh
