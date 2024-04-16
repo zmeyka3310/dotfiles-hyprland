8d2496582eb33ac356d8f8fcd274dde8ff1ce011
zmeyka3310 <castella3310@gmail.com>
2024-04-16 11:01:08 +0300

    made preloader empty by default

--- configmv.sh
+++ configmv.sh
@@ -4,3 +4,4 @@ cp -r ~/.config/wofi/ ~/repos/dotfiles-hyprland/.config/
 cp -r ~/.config/wallpapers/ ~/repos/dotfiles-hyprland/.config/
 cp -r ~/.bin/ ~/repos/dotfiles-hyprland
 echo "" > ~/repos/dotfiles-hyprland/.bin/screenshotassist.sh
+echo "" > ~/repos/dotfiles-hyprland/.config/hypr/preloader.conf
