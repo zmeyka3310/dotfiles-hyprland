ef07c583bbf2ba5395e18b8a3b57fdb8eb2a9c37
zmeyka3310 <castella3310@gmail.com>
2024-04-30 12:09:47 +0300

    added a new style

---
+++ .bin/style4.sh
@@ -0,0 +1,6 @@
+hyprctl hyprpaper wallpaper eDP-1,~/.config/wallpapers/style4.png
+echo 'source = ~/.config/hypr/style4.conf' > .config/hypr/preloader.conf
+sed -i '19s/.*/#516ced/' .config/waybar/style.css
+hyprctl reload
+pkill -f waybar
+waybar & disown
