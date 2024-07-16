da33cfe914741953d0004022b9e15d18991fb3b3
zmeyka3310 <castella3310@gmail.com>
2024-07-17 00:38:15 +0300

    added .bashrc

---
+++ .bashrc
@@ -0,0 +1,22 @@
+# .bashrc
+
+# If not running interactively, don't do anything
+[[ $- != *i* ]] && return
+
+alias ls='ls --color=auto'
+PS1='[\u@\h \W]\$ '
+. "$HOME/.cargo/env"
+
+# Added by ProtonUp-Qt
+if [ -d "/home/zmeyka/stl/prefix" ]; then export PATH="$PATH:/home/zmeyka/stl/prefix"; fi
+
+# Aliases
+alias reboot='sudo reboot'
+
+alias fetch='fastfetch'
+
+alias ll='ls -l'
+
+alias cl='clear'
+# commands to run on terminal start
+ufetch
--- configmv.sh
+++ configmv.sh
@@ -2,6 +2,8 @@ cp -r ~/.config/hypr/ ~/repos/dotfiles-hyprland/.config/
 cp -r ~/.config/waybar/ ~/repos/dotfiles-hyprland/.config/
 cp -r ~/.config/wofi/ ~/repos/dotfiles-hyprland/.config/
 cp -r ~/.config/wallpapers/ ~/repos/dotfiles-hyprland/.config/
+cp -r ~/.config/fastfetch/ ~/repos/dotfiles-hyprland/.config/
 cp -r ~/.bin/ ~/repos/dotfiles-hyprland
+cp ~/.bashrc ~/repos/dotfiles-hyprland/.bashrc
 echo "" > ~/repos/dotfiles-hyprland/.bin/screenshotassist.sh
 echo "" > ~/repos/dotfiles-hyprland/.config/hypr/preloader.conf
