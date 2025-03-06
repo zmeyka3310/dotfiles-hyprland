548a16fb0e3707848111b595b149684de7f1c7ce
zmeyka3310 <castella3310@gmail.com>
2025-03-06 13:18:42 +0300

    removed all traces of style 7

--- .bin/style7.sh
+++
@@ -1,7 +0,0 @@
-hyprctl hyprpaper wallpaper eDP-1,~/.config/wallpapers/style7.png
-hyprctl hyprpaper wallpaper HDMI-A-1,~/.config/wallpapers/style7.png
-echo 'source = ~/.config/hypr/style7.conf' > .config/hypr/preloader.conf
-sed -i '19s/.*/#d3ebf7/' .config/waybar/style.css
-hyprctl reload
-pkill -f waybar
-waybar & disown
