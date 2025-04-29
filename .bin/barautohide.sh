05b400f1448976edbb04ddc6daab9b36b9161ef4
zmeyka3310 <castella3310@gmail.com>
2025-04-29 10:18:33 +0300

    waybar starts hidden by default, hover near the top to show it

---
+++ .bin/barautohide.sh
@@ -0,0 +1,13 @@
+HIDDEN=true
+while true; do
+    sleep 0.1
+    # x=$(hyprctl cursorpos | cut -d "," -f 1) #if you use a vertical bar
+    y=$(hyprctl cursorpos | cut -d " " -f 2)
+    if [[ $y == 0 && $HIDDEN == true ]]; then
+        pkill -USR1 waybar  # Show waybar
+        HIDDEN=false
+    elif [[ $y -gt 100 && $HIDDEN == false ]]; then
+        pkill -USR2 waybar  # Hide waybar
+        HIDDEN=true
+    fi
+done
