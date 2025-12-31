031e1f3b1d401bddb77924b56f749a62d3154591
zmeyka3310 <castella3310@gmail.com>
2025-12-31 05:09:40 +0300

    added style 7

---
+++ .bin/style7.sh
@@ -0,0 +1,6 @@
+hyprctl hyprpaper wallpaper eDP-1,~/.config/wallpapers/style7.png
+hyprctl hyprpaper wallpaper HDMI-A-1,~/.config/wallpapers/style7.png
+echo 'source = ~/.config/hypr/style7.conf' > .config/hypr/preloader.conf
+sed -i '19s/.*/#a927a2/' .config/waybar/style.css 
+pkill -f waybar
+waybar & disown
